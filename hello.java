625e77cd86aeb4cc1bf7ae48ea9ab6b68a4658fa
wellingtonpleni <68903133+wellingtonpleni@users.noreply.github.com>
2022-02-18 13:16:50 -0300

    Atualização

--- hello.java
+++ hello.java
@@ -1,2 +1,4 @@
-/*Olá, Meu nome é Wellington Rufino
-As atividades dos cursos na DIO serão acrescentadas neste repositório*/
+/*
+*Olá, Meu nome é Wellington Rufino
+*As atividades dos cursos na DIO serão acrescentadas neste repositório
+*/
